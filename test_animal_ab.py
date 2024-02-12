92053d63f4b57f2a922d3a36718d11f309bf33ff
Giulio Viganò <95035641+gviga@users.noreply.github.com>
2024-02-12 13:07:31 +0100

    Update test_animal_ab.py

--- test_animal_ab.py
+++ test_animal_ab.py
@@ -36,7 +36,7 @@ def pc_normalize(pc):
     return pc
 
 
-
+#refinement algorithm
 def ab_zo(p2p_21, evects1, evects2,k_init=20, nit=10, step=1):
 
     fmap_zo = p2p_to_FM(p2p_21, evects1[:,:k_init], evects2[:,:k_init],bijective=True) #C_{YX}, A_{XY}^T
@@ -48,29 +48,24 @@ def ab_zo(p2p_21, evects1, evects2,k_init=20, nit=10, step=1):
     return p2p_zo
 
 
+#dataloading
 v=np.load('./data/animal_val_easy2.npy' ).astype(np.float32)
-#loading geodesic distance matrix (for evaluation)
-#geod_load = hdf5storage.loadmat('./data/FAUST1k/N_out.mat')
-#D = geod_load['D'].astype(np.float32)
-#
 solver=pp3d.PointCloudHeatSolver(v[0])
 A_geod=np.zeros((1000,1000))
 for i in range(v[0].shape[0]):
     A_geod[i,:]=solver.compute_distance(i)
 
+
+
+#experiment parameters
 p2p_gt = np.arange(1000)
 n_esp=100
-
-
-#landmarks
-lm=[25,234,527,777,984]
-
 k=30
-
 bijective=True
 adj=True
-#Load data-driven basis
 
+
+#LOAD MODELS
 
 #POINTNET
 pointnet_model = PointNetBasis(k=k, feature_transform=False)
@@ -99,14 +94,6 @@ pointnet2_model_desc.load_state_dict(checkpoint)
 pointnet2_model_desc = pointnet2_model_desc.eval()
 pointnet2_model_desc.cuda()
 
-'''
-pointnet2_model_desc_var = PointNet2Desc(k=40, feature_transform=False)
-checkpoint = torch.load('./models/pointnetpp/desc_model_best_animal.pth',map_location=torch.device('cuda'))
-pointnet2_model_desc_var.load_state_dict(checkpoint)
-pointnet2_model_desc_var = pointnet2_model_desc_var.eval()
-pointnet2_model_desc_var.cuda()
-
-'''
 #DALTACONV
 deltaconv_model = DeltaConvBasis(in_channels=3, k=30,  conv_channels=[64, 128, 256], mlp_depth=2, embedding_size=512, num_neighbors=20, grad_regularizer=0.001, grad_kernel_width=1)
 checkpoint = torch.load('./models/deltaconv/basis_model_best_animal.pth',map_location=torch.device('cuda'))
@@ -135,6 +122,8 @@ diffusionnet_model_desc.load_state_dict(checkpoint)
 diffusionnet_model_desc=diffusionnet_model_desc.eval()
 diffusionnet_model_desc.cuda()
 
+
+#SAVING
 match_lie=np.zeros((n_esp,p2p_gt.shape[0]))
 err_lie_geod=np.zeros((n_esp,p2p_gt.shape[0]))
 err_lie_eu=np.zeros((n_esp,p2p_gt.shape[0]))
@@ -151,11 +140,11 @@ ortho_lie_B=np.zeros((n_esp,))
 bij_lie_B=np.zeros((n_esp,))
 inj_lie_B=np.zeros((n_esp,))
 conversion_err_lie_B=np.zeros((n_esp,p2p_gt.shape[0]))
-#TEST
 
+
+
+#EXPERIMENT
 print('POINTNET:\n')
-
-
 np.random.seed(2)
 for i in range(n_esp):
     #shape selection
@@ -174,7 +163,8 @@ for i in range(n_esp):
     A_geod=np.zeros((1000,1000))
     for ll in range(v2.shape[0]):
         A_geod[ll,:]=solver.compute_distance(ll)
-       # Computing Basis and Descriptors
+    
+    # Computing Basis and Descriptors
     pred_basis = pointnet_model(torch.transpose(torch.tensor(v[[shapen1,shapen2],:,:].astype(np.float32),device='cuda'),1,2))
     basis = pred_basis[0].cpu().detach().numpy()
     pred_desc = pointnet_model_desc(torch.transpose(torch.tensor(v[[shapen1,shapen2],:,:].astype(np.float32),device='cuda'),1,2))
@@ -259,7 +249,6 @@ for i in range(n_esp):
     for ll in range(v2.shape[0]):
         A_geod[ll,:]=solver.compute_distance(ll)
 
-       # Computing Basis and Descriptors
     # Computing Basis and Descriptors
     pred_basis = pointnet2_model(torch.transpose(torch.tensor(v[[shapen1,shapen2],:,:].astype(np.float32),device='cuda'),1,2))
     basis = pred_basis.cpu().detach().numpy()
@@ -323,94 +312,7 @@ print(f'Errore medio geodesico Lie: {np.mean(np.mean(err_lie_geod,1),0):4f} \n'
       f'Errore iniettività Lie: {np.mean(inj_lie):4f} \n'
       f'Errore orthogonalità Lie: {np.mean(ortho_lie):4f} \n'
       f'Errore Conversione Lie : {np.mean(np.mean(conversion_err_lie,1),0):4f} \n')
-    
-'''
-print('POINTNET BASIS + POINET++ DESCRIPTORS')
 
-
-np.random.seed(2)
-for i in range(n_esp):
-    #shape selection
-    #vec = np.random.randint(100,size=2)
-    if bijective:
-        shapen1 = 1
-        shapen2 = 0
-    else:
-        shapen1 = 0
-        shapen2 = 1
-    #save shapes
-    v1 = pc_normalize(v[shapen1,:,:].squeeze())
-    v2 = pc_normalize(v[shapen2,:,:].squeeze())
-
-    solver=pp3d.PointCloudHeatSolver(v2)
-    A_geod=np.zeros((1000,1000))
-    for ll in range(v2.shape[0]):
-        A_geod[ll,:]=solver.compute_distance(ll)
-
-       # Computing Basis and Descriptors
-    # Computing Basis and Descriptors
-    pred_basis = pointnet_model(torch.transpose(torch.tensor(v[[shapen1,shapen2],:,:].astype(np.float32),device='cuda'),1,2))
-    basis = pred_basis[0].cpu().detach().numpy()
-    pred_desc = pointnet2_model_desc_var(torch.transpose(torch.tensor(v[[shapen1,shapen2],:,:].astype(np.float32),device='cuda'),1,2))
-    desc = pred_desc.cpu().detach().numpy()
-    # Saving basis amd descriptors
-    basis1 = np.squeeze(basis[0])
-    basis2 = np.squeeze(basis[1])
-    desc1 = np.squeeze(desc[0])
-    desc2 = np.squeeze(desc[1])
-    #map optimization
-    fmap = map_fit(basis1[:,:20],basis2[:,:20],desc1,desc2)   #from v1 to v2
-
-    #map conversion (if bijective p2p from v1 to v2), (else p2p from v2 to v1)
-    p2p = FM_to_p2p(fmap, basis1[:,:20], basis2[:,:20],adj, bijective)
-    match_lie[i,:]=p2p
-    #evaluation before refinement
-    err_lie_geod_B[i,:] =A_geod[(p2p, p2p_gt)]
-    if bijective: 
-        err_lie_eu_B[i,:] = np.sqrt(np.sum(np.square(v2[p2p_gt.astype(np.int32)] - v2[p2p,:]),1))    #euclidean error
-    else:
-        err_lie_eu_B[i,:] = np.sqrt(np.sum(np.square(v1[p2p_gt.astype(np.int32)] - v1[p2p,:]),1))     #euclidean error
-
-    bij_lie_B[i]=eval_bijectivity(p2p,p2p_gt)
-    inj_lie_B[i]=eval_injectivity2(p2p,p2p_gt)
-
-    ortho_lie_B[i]=eval_orthogonality(fmap)
-    p2p=ab_zo(p2p,basis1,basis2,20,10)
-    #evaluation module
-    err_lie_geod[i,:] =A_geod[(p2p, p2p_gt)]
-    if bijective: 
-        err_lie_eu[i,:] = np.sqrt(np.sum(np.square(v2[p2p_gt.astype(np.int32)] - v2[p2p,:]),1))    #euclidean error
-    else:
-        err_lie_eu[i,:] = np.sqrt(np.sum(np.square(v1[p2p_gt.astype(np.int32)] - v1[p2p,:]),1))     #euclidean error
-
-    bij_lie[i]=eval_bijectivity(p2p,p2p_gt)
-    inj_lie[i]=eval_injectivity2(p2p,p2p_gt)
-
-    ortho_lie[i]=eval_orthogonality(fmap)
-    
-    ortho_lie[i]=eval_orthogonality(fmap)
-    fmap_gt=p2p_to_FM(p2p_gt, basis1, basis2,bijective)
-    p2p_conv=FM_to_p2p(fmap_gt,basis1,basis2,adj,bijective)
-    conversion_err_lie[i,:]=A_geod[(p2p_conv, p2p_gt)]
-
-print('INI\n')
-
-print(f'Errore medio geodesico Lie: {np.mean(np.mean(err_lie_geod_B,1),0):4f} \n'
-      f'Errore medio euclideo Lie: {np.mean(np.mean(err_lie_eu_B,1),0):4f} \n'
-      f'Errore biettività Lie: {np.mean(bij_lie_B):4f} \n'
-      f'Errore iniettività Lie: {np.mean(inj_lie_B):4f} \n'
-      f'Errore orthogonalità Lie: {np.mean(ortho_lie_B):4f} \n'
-      f'Errore Conversione Lie : {np.mean(np.mean(conversion_err_lie_B,1),0):4f} \n')
-
-print('REF\n')
-print(f'Errore medio geodesico Lie: {np.mean(np.mean(err_lie_geod,1),0):4f} \n'
-      f'Errore medio euclideo Lie: {np.mean(np.mean(err_lie_eu,1),0):4f} \n'
-      f'Errore biettività Lie: {np.mean(bij_lie):4f} \n'
-      f'Errore iniettività Lie: {np.mean(inj_lie):4f} \n'
-      f'Errore orthogonalità Lie: {np.mean(ortho_lie):4f} \n'
-      f'Errore Conversione Lie : {np.mean(np.mean(conversion_err_lie,1),0):4f} \n')
-    
-'''
 
 print('DELTACONV:')
 
@@ -566,7 +468,6 @@ for i in range(n_esp):
     
     v1=np.squeeze(v1.cpu().detach().numpy())
     v2=np.squeeze(v2.cpu().detach().numpy())
-    #map optimization
     #map optimization
     fmap = map_fit(basis1[:,:20],basis2[:,:20],desc1,desc2)   #from v1 to v2
 
